52239c623f50e8eed82651597611bf02fcb24780
YueLi <44028107+YueLi-0816@users.noreply.github.com>
2023-09-04 12:19:51 +0100

    predict the long term traffic flow (24)
    however the temporal dependence of the residuals exist

--- SVR.R
+++ SVR.R
@@ -17,6 +17,9 @@ View(new_flow)
 m <- 3
 data <- embed(x = new_flow[,"newFlow"], dimension = m+1)
 colnames(data) <- c("y_t-3","y_t-2","y_t-1","y_t")
+View(data)
+class(data)
+
 
 #In non-temporal data the training and testing sets are usually divided randomly. 
 #However, with temporal data it is important to consider the ordering of time.
@@ -34,6 +37,7 @@ yTest <- data[(split+1):nrow(data),1]
 XTest <- data[(split+1):nrow(data),-1]
 
 View(XTrain)
+class(XTrain)
 
 #use k-fold cross validation, with k set to 5.
 library(sp)
@@ -84,4 +88,86 @@ plot(SVRResidual)
 
 acf(SVRResidual)
 
+
+
+
+
+#------------------------
+#do the long-term traffic flow prediction (24h)
+start_long <- which(new_flow$timeStamp == '2019-11-22 00:00:00')
+end_long <- which(new_flow$timeStamp == '2019-12-27 23:45:00')
+yLong <- new_flow[start_long:nrow(new_flow),'newFlow',drop=FALSE]
+colnames(yLong) <- 'y' 
+row.names(yLong) <- NULL  #reset the index
+xLong <- data.frame(embed(x = new_flow[1:(end_long+3), 'newFlow'], dimension = m+1))
+colnames(xLong) <- c("y_t-21","y_t-22","y_t-23","y_t-24")
+class(yLong)
+
+dataLong <- data.matrix(cbind(yLong, xLong))
+
+View(dataLong)
+class(dataLong)
+
+#split the data into trainset and test set
+n <- nrow(dataLong)
+trainDays <- ceiling(n*0.8/96)
+split <- 96*trainDays
+
+yTrain <- dataLong[1:split,1]
+XTrain <- dataLong[1:split,-1]
+yTest <- dataLong[(split+1):nrow(dataLong),1]
+XTest <- dataLong[(split+1):nrow(dataLong),-1]
+
+View(yTrain)
+class(XTrain)
+
+#use k-fold cross validation, with k set to 5.
+library(sp)
+#install.packages('caret')
+#install.packages('rlang')
+library(ggplot2)
+library(caret)
+ctrl <- trainControl(method = "cv", number=5) 
+
+#Create a grid of parameters to test and train the model:
+SVRGridCoarse <- expand.grid(.sigma=c(0.001, 0.01, 0.1), .C=c(10,100,1000))
+SVRFitCoarse <- train(XTrain, yTrain, method="svmRadial", tuneGrid=SVRGridCoarse
+                      , trControl=ctrl, type="eps-svr")
+SVRFitCoarse
+# root mean squared error (RMSE), R squared and Mean Absolute Error (MAE).
+
+plot(SVRFitCoarse)
+
+# refine the grid to see if we can gain further improvements in performance:
+SVRGridFine <- expand.grid(.sigma=c(0.05, 0.01, 0.015), .C=c(500,1000,1500))
+system.time(SVRFitFine <- train(XTrain, yTrain, method="svmRadial", tuneGrid=SVRGridFine, 
+                                trControl=ctrl, type="eps-svr"))
+
+SVRFitFine
+plot(SVRFitFine)
+
+
+#Examine the properties of the fitted model:
+names(SVRFitFine)
+#The model object for the best model can be accessed by
+SVRFitFine$finalModel
+
+SVRFitFine
+
+#use the model for one-step-ahead prediction and plot the results
+yPred <- predict(SVRFitCoarse, XTest)
+plot(yTest, type="l", xaxt="n", xlab="Date", ylab="UKTemp")
+lines(yPred, col="red", lwd=2)
+points(yPred, col="blue", pch=21, bg="blue")
+#axis(1, at=seq(90, (180*5)+90, 180), labels=unique(dates[(split+1):nrow(data),1]))
+axis(1, at=seq(6, (12*19)+6, 12), labels=format(dates[(trainYears+1):length(dates)], "%Y"))
+legend(90, 3, legend=c("Observed"), lty=1, bty="n")
+legend(150, 3, legend=c("Predicted"), pch=21, col="blue", bg="blue", bty="n")
+
+
+#check whether any temporal autocorrelation remains in the residuals of the model:
+SVRResidual <- yTest-yPred
+plot(SVRResidual)
+
+acf(SVRResidual)
 
